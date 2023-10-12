f140e1a24e023f21a220df887f0ba231e8a957bf
mdatif796 <mdatif796@gmail.com>
2023-10-12 13:44:13 +0530

    created post schema

---
+++ models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
